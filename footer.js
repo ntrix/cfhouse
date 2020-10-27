9d7cab1f6682a9c58f4d61fb45f014a35d2fb23a
ntrix <ntrix@ntien.com>
2020-10-27 15:25:37 +0100

    js extract common footer, html correct prettier

---
+++ footer.js
@@ -0,0 +1,60 @@
+$('#footer').append(`
+    <div class="container">
+        <div class="row d-flex">
+            <div class="col-sm-4 col-xs-12">
+                <a href="index.html">
+                    <img
+                        src="images/footerLogo.png"
+                        loading="lazy"
+                        alt="footer logo"
+                        id="footer__logo"
+                    />
+                </a>
+            </div>
+
+            <div class="col-sm-3 col-xs-6 footer__contacts" id="contact">
+                <h4><b>Coffee 76 eCommerce Ltd.</b></h4>
+                <p>
+                    Hotline<br />
+                    <b>1900 1234 - 5678 000</b><br />
+                    <br />
+                    Address:<br />
+                    123 ABC street
+                </p>
+            </div>
+
+            <div class="col-sm-2 col-xs-4 footer__links">
+                <ul>
+                    <li><a href="#page-top">About Us</a></li>
+                    <li><a href="product1.html#filter">Menu</a></li>
+                    <li><a href="#news">News</a></li>
+                    <li><a href="#footer">Carrier</a></li>
+                    <li><a href="#contacts">Contact</a></li>
+                </ul>
+            </div>
+
+            <div class="col-sm-3 col-xs-2">
+                <ul class="social">
+                    <li>
+                        <a href="https://www.facebook.com"
+                            ><i class="fab fa-facebook"></i
+                        ></a>
+                    </li>
+                    <li>
+                        <a href="https://www.instagram.com"
+                            ><i class="fab fa-instagram"></i
+                        ></a>
+                    </li>
+                    <li>
+                        <a href="https://twitter.com"
+                            ><i class="fab fa-twitter"></i
+                        ></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="row text-center">
+            <p>2019 Copyright &copy; Coffee 76. Design by NT T</p>
+        </div>
+    </div>`
+);
