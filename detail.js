707524108bc6bb860bd4fc89192c5038eb9ca4d3
ntrix <ntrix@ntien.com>
2020-10-27 13:46:12 +0100

    extract js to extern .js files

---
+++ detail.js
@@ -0,0 +1,93 @@
+const F = document.querySelector.bind(document);
+$("#shadow--mobile").hide();
+$("#nav--mobile").hide();
+$("#search__field").hide();
+
+const products = [
+    "imgProduct2",
+    "imgProduct3",
+    "imgProduct4",
+    "imgProduct5",
+    "imgProduct6",
+    "productLatte",
+];
+for (let product of products) {
+    $("#js_carousel__1").append(`
+<div class="owl--slides">
+<a><img src="images/detail/${product}.jpg" loading="lazy" alt="${product}" class="img-thumbnail"/></a>
+</div>
+`);
+}
+
+const others = [
+    "product1",
+    "product2",
+    "product3",
+    "product4",
+    "product2",
+    "product4",
+];
+for (let other of others) {
+    $("#js_carousel__2").append(`
+<div class="owl--slides2">
+<a href="product.html"><img src="images/detail/${other}.jpg" loading="lazy" alt="${other}" class="img-responsive"/></a>
+</div>
+`);
+}
+
+$("#js_hamburger-menu").click(function () {
+    $("#search__field").fadeOut();
+    $("#shadow--mobile").fadeToggle();
+    $("#nav--mobile").slideToggle();
+});
+
+$("#shadow--mobile").click(function () {
+    $("#nav--mobile").slideUp();
+    $("#shadow--mobile").fadeOut();
+});
+
+$("#search__icon").click(function () {
+    $("#search__field").fadeToggle();
+    F("#search__field").value = "";
+});
+
+$("#search__field").keypress(function (event) {
+    if (event.keyCode == "13") {
+        F("#search__field").value = "";
+        $("#search__field").fadeOut();
+    }
+});
+
+$("#js_carousel__1").owlCarousel({
+    dots: false,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 2900,
+    autoplayHoverPause: true,
+    smartSpeed: 1000,
+    responsiveClass: true,
+    responsive: {
+        0: { items: 3, center: true },
+        768: { items: 3 },
+        992: { items: 4 },
+    },
+});
+
+$("#js_carousel__2").owlCarousel({
+    dots: false,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 3100,
+    autoplayHoverPause: true,
+    smartSpeed: 1000,
+    responsiveClass: true,
+    responsive: {
+        0: { items: 2 },
+        768: { items: 4 },
+    },
+});
+
+$("#js_carousel__1 img").click(function () {
+    let imgSrc = $(this).attr("src");
+    $("#large-box img").attr("src", imgSrc);
+});
---
+++ product.js
@@ -0,0 +1,89 @@
+const F = document.querySelector.bind(document);
+const linkDetail = "detail.html#",
+    linkAdd = "#",
+    linkOrder = "indexJS.html#payment";
+
+$("#shadow--mobile").hide();
+$("#nav--mobile").hide();
+$("#search__field").hide();
+
+const imagesPath = "images/product/";
+function renderItems(sectionName, items) {
+    $("#js_main").append(`
+<section class="row d-flex" id="${sectionName}">
+<div class="col-xs-12 section__label">&diams; ${sectionName.toUpperCase()}</div>
+</section>`);
+    for (let item of items) {
+        $("#" + sectionName).append(`
+<div id="${item}" class="product-card card--actions">
+<img loading="lazy" alt="${item}" src="${
+            imagesPath + item
+        }.jpg">
+<div class="card--actions__overlay">
+    <div><a href="${
+            linkDetail + item
+        }" class="actions__btn">Details</a></div>
+    <div><a href="${
+            linkAdd + item
+        }" class="actions__btn">Add to cart</a></div>
+    <div><a href="${linkOrder}" class="actions__btn">Order</a></div>
+</div>
+</div>`);
+    }
+}
+
+const specialties = [
+    "specialty1",
+    "lycheeTea",
+    "specialty3",
+    "specialty4",
+    "specialty5",
+    "specialty6",
+];
+renderItems("specialties", specialties);
+
+const expresso = [
+    "expresso1",
+    "expresso2",
+    "expresso3",
+    "expresso4",
+    "expresso5",
+];
+renderItems("expresso", expresso);
+
+const latte = ["latte1", "latte2", "latte3"];
+renderItems("latte", latte);
+
+const mocktail = [
+    "mocktail1",
+    "mocktail2",
+    "mocktail3",
+    "mocktail4",
+];
+renderItems("mocktail", mocktail);
+
+const dessert = ["dessert1", "dessert2", "dessert3"];
+renderItems("dessert", dessert);
+
+$("#js_hamburger-menu").click(function () {
+    $("#search__field").fadeOut();
+    $("#shadow--mobile").fadeToggle();
+    $("#nav--mobile").slideToggle();
+});
+
+$("#shadow--mobile").click(function () {
+    $("#nav--mobile").slideUp();
+    $("#shadow--mobile").fadeOut();
+});
+
+$("#search__icon").click(function () {
+    $("#search__field").fadeToggle();
+    F("#search__field").value = "";
+});
+
+$("#search__field").keypress(function (event) {
+    if (event.keyCode == "13") {
+        F("#search__field").value = "";
+        $("#search__field").fadeOut();
+    }
+});
